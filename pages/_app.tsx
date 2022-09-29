6abce382365b7aed4864860854ae5d6c7d760455
darcdev <diegoandresrojas2000@gmail.com>
2022-09-29 01:20:06 -0500

    feat: add sidebar menu

---
+++ pages/_app.tsx
@@ -0,0 +1,42 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "reset-css";
+import PlayerLayout from "../components/PlayerLayout";
+
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: "#f5f5f5",
+      200: "#EEEEEE",
+      300: "#E0E0E0",
+      400: "#BDBDBD",
+      500: "#757575",
+      600: "#616161",
+      800: "#424242",
+      900: "#212121",
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ":focus": {
+            outline: "none",
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+  },
+});
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <PlayerLayout>
+        <Component {...pageProps} />
+      </PlayerLayout>
+    </ChakraProvider>
+  );
+};
+
+export default MyApp;
