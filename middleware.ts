6450583ffdeb645e0a3de1ce5845a94224cea3c3
darcdev <diegoandresrojas2000@gmail.com>
2022-11-03 22:13:19 -0500

    feat : add verifcation token middleware on edge function

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @next/next/no-server-import-in-page */
+import { NextResponse } from "next/server";
+
+const signedinPages = ["/", "/playlist", "/library"];
+
+export default function middleware(req) {
+  if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
+    const token = req.cookies.TRAX_ACCESS_TOKEN;
+
+    if (!token) {
+      const loginUrl = new URL("/signin", req.url);
+      return NextResponse.redirect(loginUrl);
+    }
+  }
+}
