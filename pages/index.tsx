e9100cbc9d505d600e4a3c8daec0ba67f9796969
darcdev <diegoandresrojas2000@gmail.com>
2022-11-09 00:42:51 -0500

    feat : add background layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,17 @@
+import GradientLayout from "../components/gradientLayout";
+
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <GradientLayout
+      color="red"
+      roundImage
+      subtitle="profile"
+      title="Scott Moss"
+      description="15 public playlists"
+      image="https://frontendmasters.github.io/fullstack-app-next-website/images/profile.png"
+    >
+      <div>Home page</div>
+    </GradientLayout>
+  );
 };
 export default Home;
